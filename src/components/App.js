11794879f9f91c4fb737986d96a73b8a8ee06126
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2021-12-25 13:10:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );

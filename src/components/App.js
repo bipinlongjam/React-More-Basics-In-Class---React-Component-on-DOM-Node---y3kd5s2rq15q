ec7d31a0271fbb66e82d323fa522a4dd66bc5814
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2021-12-25 12:43:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <>
-      <div class="container">
+    
         <h1> Enter the Text to Analyze" </h1>
-      </div>
-    </>
+     
   );
 }
 

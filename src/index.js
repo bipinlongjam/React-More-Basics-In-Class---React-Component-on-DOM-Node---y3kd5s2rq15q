2c8dc8244e6763278dda2d21dcdc1aa6355c82ba
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2021-12-25 12:17:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-function ele(){
-    const element =(
-        <div>
-        <App/>
-        </div>
-        );
-    ReactDOM.render(element,document.getElementById('root'));
+
+    ReactDOM.render(<App/>,document.getElementById('root'));
 }

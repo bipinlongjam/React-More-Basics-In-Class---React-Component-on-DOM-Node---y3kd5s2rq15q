728b3ac87fa1b16121d7722b1e4da1c54b3ce3b7
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2022-01-29 22:14:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-
-    ReactDOM.render(<App/>,rootElement);
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
